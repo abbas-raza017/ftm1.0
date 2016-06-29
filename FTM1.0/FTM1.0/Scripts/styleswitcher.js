156c8f03ee9b562d68d8dfd9e6bda3310f0b02d3
abbas-raza017 <abbas.raza.jafri017@gmail.com>
2016-06-29 22:56:26 +0500

    Color switcher issue

--- FTM1.0/FTM1.0/Scripts/styleswitcher.js
+++ FTM1.0/FTM1.0/Scripts/styleswitcher.js
@@ -1,23 +1,22 @@
-if($.cookie("color")) {
-    $("link[href|='Content/css/color']").attr("href", "Content/css/" + $.cookie("color"));
+if ($.cookie("color")) {
+    $("#colorSwitchLink").attr("href", "/Content/css/" + $.cookie("color"));
 }
 
-if($.cookie("width")) {
-    $("link[href|='Content/css/width']").attr("href", "Content/css/" + $.cookie("width"));
+if ($.cookie("width")) {
+    $("link[href|='Content/css/width']").attr("href", "/Content/css/" + $.cookie("width"));
 }
 
-$(document).ready(function() { 
+$(document).ready(function () {
     $("#color-switcher-content .color").click(function () {
-        console.log($("link[href|='Content/css/color']"));
-        $("link[href|='Content/css/color']").attr("href", "~/Content/css/" + $(this).attr('rel'));
-        $.cookie("color",$(this).attr('rel'), {expires: 7, path: '/'});
+        $("#colorSwitchLink").attr("href", "/Content/css/" + $(this).attr('rel'));
+        $.cookie("color", $(this).attr('rel'), { expires: 7, path: '/' });
         return false;
     });
 
-    $("#color-switcher-content .option").click(function() { 
-        $("link[href|='Content/css/width']").attr("href", "Content/css/" + $(this).attr('rel'));
+    $("#color-switcher-content .option").click(function () {
+        $("link[href|='Content/css/width']").attr("href", "/Content/css/" + $(this).attr('rel'));
         console.log('test');
-        $.cookie("width",$(this).attr('rel'), {expires: 7, path: '/'});
+        $.cookie("width", $(this).attr('rel'), { expires: 7, path: '/' });
         return false;
     });
 });
